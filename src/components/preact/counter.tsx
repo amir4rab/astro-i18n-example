245320d4b734e1d11abc7da18cec9a8b8e1f0554
Amir <60965542+amir4rab@users.noreply.github.com>
2023-09-25 05:59:21 +0000

    added dark mode

--- src/components/preact/counter.tsx
+++ src/components/preact/counter.tsx
@@ -8,24 +8,24 @@ const Counter = () => {
   const [value, setValue] = useState(0);
 
   return (
-    <div class="bg-neutral-200 p-4 rounded-xl relative">
+    <div class="bg-neutral-200 dark:bg-neutral-800 p-4 rounded-[calc(theme(borderRadius.3xl)_-_1rem)] relative">
       <h2 class="mt-0 mb-4">{`${t("counter")}: ${value}`}</h2>
       <div class="flex justify-center items-center gap-2">
         <button
-          class="bg-neutral-300 rounded-xl p-2 aspect-square overflow-hidden block leading-none w-8"
+          class="bg-neutral-300 dark:bg-neutral-700 rounded-xl p-2 aspect-square overflow-hidden block leading-none w-8"
           onClick={() => setValue((curr) => curr - 1)}
         >
           -
         </button>
         <p class="my-0">{value}</p>
         <button
-          class="bg-neutral-300 rounded-xl p-2 aspect-square overflow-hidden block leading-none w-8"
+          class="bg-neutral-300 dark:bg-neutral-700 rounded-xl p-2 aspect-square overflow-hidden block leading-none w-8"
           onClick={() => setValue((curr) => curr + 1)}
         >
           +
         </button>
       </div>
-      <p className="text-xs mx-auto bg-green-200 p-2 rounded-xl block absolute top-4 right-4 font-mono leading-none my-0">
+      <p className="text-xs mx-auto bg-green-200 dark:bg-green-800 p-2 rounded-xl block absolute top-4 right-4 font-mono leading-none my-0">
         Preact
       </p>
     </div>
--- src/components/react/counter.tsx
+++ src/components/react/counter.tsx
@@ -6,24 +6,24 @@ const Counter = () => {
   const { t } = useTranslations();
 
   return (
-    <div className="bg-neutral-200 p-4 rounded-xl relative">
+    <div className="bg-neutral-200 dark:bg-neutral-800 p-4 rounded-[calc(theme(borderRadius.3xl)_-_1rem)] relative">
       <h2 className="mt-0 mb-4">{`${t("counter")}: ${value}`}</h2>
       <div className="flex justify-center items-center gap-2">
         <button
-          className="bg-neutral-300 rounded-xl p-2 aspect-square overflow-hidden block leading-none w-8"
+          className="bg-neutral-300 dark:bg-neutral-700 rounded-xl p-2 aspect-square overflow-hidden block leading-none w-8"
           onClick={() => setValue((curr) => curr - 1)}
         >
           -
         </button>
         <p className="my-0">{value}</p>
         <button
-          className="bg-neutral-300 rounded-xl p-2 aspect-square overflow-hidden block leading-none w-8"
+          className="bg-neutral-300 dark:bg-neutral-700 rounded-xl p-2 aspect-square overflow-hidden block leading-none w-8"
           onClick={() => setValue((curr) => curr + 1)}
         >
           +
         </button>
       </div>
-      <p className="text-xs mx-auto bg-green-200 p-2 rounded-xl block absolute top-4 right-4 font-mono leading-none my-0">
+      <p className="text-xs mx-auto bg-green-200 dark:bg-green-800 p-2 rounded-xl block absolute top-4 right-4 font-mono leading-none my-0">
         React
       </p>
     </div>
